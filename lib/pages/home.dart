a2b94f7c238ce60912b82327224e1140632916d4
ReemWSW <widsawa.d31@gmail.com>
2022-12-11 23:20:14 +0700

    [HOME] create homepage empty

---
+++ lib/pages/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ยาของฉัน'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
