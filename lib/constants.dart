d93c7808dcf406d726b3394da602cc4f78302153
ReemWSW <widsawa.d31@gmail.com>
2022-12-14 11:16:32 +0700

    constant color

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+const kPrimaryColor = Color(0xFF60C7C1);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF8F8F6);
+
+const double kDefaultPadding = 20.0;
