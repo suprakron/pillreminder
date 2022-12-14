364eb1c7a15b24c9c475184123641c1d963ce311
ReemWSW <widsawa.d31@gmail.com>
2022-12-14 10:13:17 +0700

    [HOME] date horizontal

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int selectedIndex = 0;
+  DateTime now = DateTime.now();
+  late DateTime lastDayOfMonth;
+  @override
+
+  void initState() {
+    lastDayOfMonth = DateTime.now();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('ยาของฉัน'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+        backgroundColor: Colors.white,
+        toolbarHeight: 148.0,
+        title: Column(
+          children: [
+            Row(
+              children: const [
+                Expanded(
+                    child: Text("ตารางกินยา",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                        )))
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              physics: const ClampingScrollPhysics(),
+              child: Row(
+                children: List.generate(
+                  lastDayOfMonth.day,
+                      (index) {
+                    final currentDate =
+                    lastDayOfMonth.add(Duration(days: index + 1));
+                    final dayName = DateFormat('EE').format(currentDate);
+                    return Padding(
+                      padding: EdgeInsets.only(
+                          left: index == 0 ? 16.0 : 0.0, right: 16.0),
+                      child: GestureDetector(
+                        onTap: () => setState(() {
+                          selectedIndex = index;
+                        }),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 42.0,
+                              width: 42.0,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: selectedIndex == index
+                                    ? Colors.orange
+                                    : Colors.transparent,
+                                borderRadius: BorderRadius.circular(44.0),
+                              ),
+                              child: Text(
+                                dayName.substring(0, 1),
+                                style: TextStyle(
+                                  fontSize: 24.0,
+                                  color: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors.black54,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              "${index + 1}",
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.black54,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Container(
+                              height: 2.0,
+                              width: 28.0,
+                              color: selectedIndex == index
+                                  ? Colors.orange
+                                  : Colors.transparent,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
