cfe5ae04d27555a5ff031f3b769c628e47478cde
ReemWSW <widsawa.d31@gmail.com>
2022-12-14 11:17:10 +0700

    [HOME] SPLIT widget date horizontal

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../constants.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -9,14 +11,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int selectedIndex = 0;
   DateTime now = DateTime.now();
-  late DateTime lastDayOfMonth;
+  late DateTime dateTime;
   @override
-
   void initState() {
-    lastDayOfMonth = DateTime.now();
+    dateTime = DateTime.now();
+    selectedIndex = dateTime.day - 1;
     super.initState();
   }
 
@@ -29,86 +30,93 @@ class _MyHomePageState extends State<MyHomePage> {
         toolbarHeight: 148.0,
         title: Column(
           children: [
-            Row(
-              children: const [
-                Expanded(
-                    child: Text("ตารางกินยา",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                        )))
-              ],
-            ),
+            titleAppbar(),
             const SizedBox(height: 16.0),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              physics: const ClampingScrollPhysics(),
-              child: Row(
-                children: List.generate(
-                  lastDayOfMonth.day,
-                      (index) {
-                    final currentDate =
-                    lastDayOfMonth.add(Duration(days: index + 1));
-                    final dayName = DateFormat('EE').format(currentDate);
-                    return Padding(
-                      padding: EdgeInsets.only(
-                          left: index == 0 ? 16.0 : 0.0, right: 16.0),
-                      child: GestureDetector(
-                        onTap: () => setState(() {
-                          selectedIndex = index;
-                        }),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 42.0,
-                              width: 42.0,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: selectedIndex == index
-                                    ? Colors.orange
-                                    : Colors.transparent,
-                                borderRadius: BorderRadius.circular(44.0),
-                              ),
-                              child: Text(
-                                dayName.substring(0, 1),
-                                style: TextStyle(
-                                  fontSize: 24.0,
-                                  color: selectedIndex == index
-                                      ? Colors.white
-                                      : Colors.black54,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Text(
-                              "${index + 1}",
-                              style: const TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.black54,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Container(
-                              height: 2.0,
-                              width: 28.0,
-                              color: selectedIndex == index
-                                  ? Colors.orange
-                                  : Colors.transparent,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+            dateHorizontal(),
           ],
         ),
       ),
+    );
+  }
+
+  SingleChildScrollView dateHorizontal() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: const ClampingScrollPhysics(),
+      child: Row(
+        children: List.generate(
+          DateTime(now.year, now.month + 1, 0).day,
+          (index) {
+            final dateShow = dateTime.add(Duration(days: index));
+            final dayName = DateFormat('E').format(dateShow);
+            return Padding(
+              padding:
+                  EdgeInsets.only(left: index == 0 ? 16.0 : 0.0, right: 16.0),
+              child: GestureDetector(
+                onTap: () => setState(() {
+                  selectedIndex = index;
+                }),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 42.0,
+                      width: 42.0,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: selectedIndex == index
+                            ? kPrimaryColor
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(44.0),
+                      ),
+                      child: Text(
+                        dayName,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: selectedIndex == index
+                              ? Colors.white
+                              : Colors.black54,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      "${index + 1}",
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Container(
+                      height: 2.0,
+                      width: 28.0,
+                      color: selectedIndex == index
+                          ? kPrimaryColor
+                          : Colors.transparent,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Row titleAppbar() {
+    return Row(
+      children: const [
+        Expanded(
+            child: Text("ตารางกินยา",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                )))
+      ],
     );
   }
 }
