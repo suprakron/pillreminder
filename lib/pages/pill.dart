5507b6dca2c6cb76b742cc5bbf985d6bf45a8eb6
ReemWSW <widsawa.d31@gmail.com>
2022-12-19 11:27:13 +0700

    remove safearea in pill page

--- lib/pages/pill.dart
+++ lib/pages/pill.dart
@@ -15,42 +15,40 @@ class PillPage extends StatefulWidget {
 class _PillPageState extends State<PillPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('รายการยา'),
-          backgroundColor: kPrimaryColor,
-        ),
-        body: Container(
-          color: kBackgroundColor,
-          margin: const EdgeInsets.only(top: 10, left: 8, right: 8),
-          child: FutureBuilder(
-            future: DatabaseHelper.queryAllRows(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  List<Map<String, dynamic>> rows = snapshot.data;
-                  return ListView.builder(
-                    itemCount: rows.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      PillModel pill = PillModel.fromJson(rows[index]);
-                      return PillList(
-                        pillModel: pill,
-                        onDelete: () {
-                          setState(() {});
-                        },
-                      );
-                    },
-                  );
-                }
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('รายการยา'),
+        backgroundColor: kPrimaryColor,
+      ),
+      body: Container(
+        color: kBackgroundColor,
+        margin: const EdgeInsets.only(top: 10, left: 8, right: 8),
+        child: FutureBuilder(
+          future: DatabaseHelper.queryAllRows(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
               } else {
-                return const Center(child: CircularProgressIndicator());
+                List<Map<String, dynamic>> rows = snapshot.data;
+                return ListView.builder(
+                  itemCount: rows.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    PillModel pill = PillModel.fromJson(rows[index]);
+                    return PillList(
+                      pillModel: pill,
+                      onDelete: () {
+                        setState(() {});
+                      },
+                    );
+                  },
+                );
               }
-            },
-          ),
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ),
       ),
     );
